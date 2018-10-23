b5c85614f4ed8a5c133e224e28eba554af95c3ce
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2018-10-22 19:36:30 -0700

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,1 +1,63 @@
+package DerivativeCalculator;
 
+import java.util.ArrayList;
+
+public class Function {
+	
+	private String input;
+	private String current;
+	private String output = "Not Found Yet";
+	private ArrayList<String> key;
+	private ArrayList<String> parts;
+	
+	public Function(String input) {
+		this.input = input;
+		current = input;
+	} 
+	
+	public String getDerivative() {
+		
+		return output;
+	}
+	
+	public String formatFunction() {
+		current = DerivativeStringFormater.stringFormater(current, key, parts);
+		return current;
+	}
+	
+	public String getInput() {
+		return input;
+	}
+	
+	public String getCurrent() {
+		return current;
+	}
+	
+	public String getOutput() {
+		return output;
+	}
+	
+	public ArrayList<String> getKey(){
+		return key;
+	}
+	
+	public ArrayList<String> getParts(){
+		return parts;
+	}
+
+	public void printKey() {
+		for(int x = 0; x < key.size(); x++) {
+			System.out.println(key.get(x));
+		}
+	}
+	
+	public void printParts() {
+		for(int x = 0; x < parts.size(); x++) {
+			System.out.println(parts.get(x));
+		}
+	}
+	
+	public String toString() {
+		return "Input Function: "+input+" Current Function: "+current+" Output Function: "+output;
+	}
+}
