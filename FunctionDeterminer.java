5f67c57a0d34c4ae8a7d3582063abaeb4d9464b0
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2018-11-04 00:26:16 -0700

    Update FunctionDeterminer.java

--- FunctionDeterminer.java
+++ FunctionDeterminer.java
@@ -1,5 +1,20 @@
-package DerivativeCalculator;
+ppackage DerivativeCalculator;
 
 public class FunctionDeterminer {
 
+	private boolean applyProductRule(String function) {
+		if(function.contains("*")) return true;
+		return false;
+	}
+	
+	private boolean applyPowerRule(String function) {
+		if(function.contains("^")) return true;
+		return false;
+	}
+	
+	private boolean applyQuotientRule(String function) {
+		if(function.contains("/")) return true;
+		return false;
+	}
+	
 }
