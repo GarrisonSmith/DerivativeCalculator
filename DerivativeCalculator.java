15cd09c75e855b6f203525a1d8e6e042f5952ca1
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2018-12-06 11:40:40 -0700

    Update DerivativeCalculator.java

--- DerivativeCalculator.java
+++ DerivativeCalculator.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class DerivativeCalculator {
 	
+	package DerivativeCalculator;
+
+import java.util.Scanner;
+
+public class DerivativeCalculator {
+	
 	public static void main (String[] args) {
 		
 		
@@ -19,7 +25,8 @@ public class DerivativeCalculator {
 			if(function.getInput().equals("Directions")) {
 				System.out.println("\n1. Do not use "_" or "i" at all ever.
 				\n2. Only sin, cos, tan, sec, csc, & cot of sinusoidal functions are supported.
-				\n3. Put ALL exponents into parenthesis.
+				\n3. Put all exponents that are more then 1 character into parenthesis, otherwise dont.
+				\n4. If it doesnt work, then you put it in wrong.
 				\n");
 			}
 			else {
@@ -35,25 +42,33 @@ public class DerivativeCalculator {
 		scan.close(); 
 		*/
 		
-		function.setInput("5x+(132x/(53x*351x))/5118x*51x-81x+sin(12354x-51x)^(-10x)-e^(x)");
-		//function.setInput("5x*4x");
+		//Example 1
+		//function.setInput("((5x)*(4x))");
 		
-		System.out.println(FunctionDeterminer.applyRule("5x/4x^22"));
+		//Example 2
+		//function.setInput("((5x)^(4x))");
 		
-		/*
-		test = DerivativeStringHandler.parentheisGrouper(test);
-	
-		System.out.println(test);
-		*/
+		//Example 3
+		//function.setInput("(ln(5x))");
+		
+		//Example 4
+		function.setInput("((ln(5x))+((5x)*(4x)))");
+		
+		//Example 5
+		//function.setInput("(cot(12x))");
 		
 		function.formatFunction();
-		/*
+		function.fillDerivativeKey();
+		
 		System.out.println("Function:");
 		System.out.println(function);
 		
 		System.out.println("\nValues in key:");
 		function.printKey();
 		
+		System.out.println("\nValues in derivativeKey:");
+		function.printDerivativeKey();
+		/*
 		System.out.println("\nValues in parts:");
 		function.printParts();
 		
@@ -71,42 +86,18 @@ public class DerivativeCalculator {
 		*/
 		//System.out.println(DerivativeStringHandler.iExpander(function.getCurrent(), function.getKey()));
 		
-		function.takeDerivative();
+		//function.takeDerivative();
 		
 		System.out.println("\nFunction:");
 		System.out.println(function);
 		System.out.println("Parts Put Together But Not Expanded");
 		System.out.println(function.constructFunction(false)); 
-		System.out.println("Parts Put Together And Expanded");
-		System.out.println(function.constructFunction(true));  
+		//System.out.println("Parts Put Together And Expanded");
+		//System.out.println(function.constructFunction(true));  
+		System.out.println("DerivativeParts Put Together And Not Expanded");
+		System.out.println(function.constructDerivative(false));
+		System.out.println("DerivativeParts Put Together And Expanded");
+		System.out.println(function.constructDerivative(true));
 		
-		/*
-		String functionS = function.getCurrent();
-		
-		System.out.println(functionS);
-		
-		int start = 0;
-		int end = 0;
-		
-		while(functionS.contains("_")) {
-			
-			for(start = 0; start < functionS.length(); start++) {
-				
-				if(functionS.charAt(start) == '_') {
-					
-					for(end = start+1; end < functionS.length(); end++) {
-						
-						if(functionS.charAt(end) == '_') {
-							//function = function.substring(0, start) + key.getFunction(function.substring(start, end) + function.substring(end, function.length()));
-							System.out.println(functionS.substring(start, end+1));
-							functionS = function.getKey().getFunction(functionS.substring(start, end+1));
-						}
-					}
-				}
-			}
-		}
-		
-		System.out.println(functionS);
-		*/
 	}
 }
